ca24450991ed00b9a1c342a0ff59a480e4e20508
Saurabh Dutta <saurabhdutta.jk@gmail.com>
2024-02-12 03:36:43 +0530

    Desktop Menu Top

--- src/components/DeskMenu.tsx
+++ src/components/DeskMenu.tsx
@@ -1,17 +1,17 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import * as React from "react"
-import DropMenu from "./DropMenu"
 import useCommon from "../hooks/useCommon"
 import { Link } from "gatsby"
+import { ChevronDownIcon } from "@heroicons/react/20/solid"
+const DeskMenu = ({
+  serviceMenuOpen,
+  setServiceMenuOpen,
+}: {
+  serviceMenuOpen: boolean | null
+  setServiceMenuOpen: React.Dispatch<React.SetStateAction<boolean | null>>
+}): React.ReactElement => {
+  const { header } = useCommon()
 
-// type DeskMenuProps = {
-//   navItems: {
-//     item: string
-//     link: string
-//   }[]
-//   services: string[]
-// }
-const DeskMenu = (): React.ReactElement => {
-  const { header, services } = useCommon()
   return (
     <>
       <nav>
@@ -22,8 +22,25 @@ const DeskMenu = (): React.ReactElement => {
               style={{ display: "flex", alignItems: "center" }}
             >
               {el?.item === "Services" ? (
-                // <Dropdown items={services} />
-                <DropMenu items={services} />
+                <button
+                  className="dropdown-button"
+                  onMouseEnter={(e) => {
+                    e.preventDefault()
+                    if (!serviceMenuOpen) setServiceMenuOpen(true)
+                  }}
+                  onClick={(e) => {
+                    e.preventDefault()
+                    setServiceMenuOpen(!serviceMenuOpen)
+                  }}
+                  style={{ display: "flex", alignItems: "center" }}
+                >
+                  Services
+                  <ChevronDownIcon
+                    className={serviceMenuOpen! ? "rotate-down" : ""}
+                    style={{ height: "18px", width: "18px" }}
+                    aria-hidden="true"
+                  />
+                </button>
               ) : (
                 <Link to={el?.link!}>{el?.item}</Link>
               )}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,15 +5,20 @@ import { useBreakpoint } from "gatsby-plugin-breakpoints"
 import MobMenu from "./MobMenu"
 import DeskMenu from "./DeskMenu"
 import { Popover } from "@headlessui/react"
+import _, { debounce } from "lodash"
 
 const Navbar = ({
   setMobileMenuActive,
 }: {
   setMobileMenuActive: React.Dispatch<React.SetStateAction<boolean>>
 }): React.ReactElement => {
-  const { header } = useCommon()
+  const { header, services } = useCommon()
   const breakpoint = useBreakpoint()
+  const debouncedMouseLeave = debounce(() => { setServiceMenuOpen(false) }, 500)
 
+  const [serviceMenuOpen, setServiceMenuOpen] = React.useState<boolean | null>(
+    null
+  )
   return (
     <Popover>
       {({ open }) => {
@@ -48,7 +53,12 @@ const Navbar = ({
                 />
               </Link>
 
-              {!breakpoint.sm && <DeskMenu />}
+              {!breakpoint.sm && (
+                <DeskMenu
+                  serviceMenuOpen={serviceMenuOpen}
+                  setServiceMenuOpen={setServiceMenuOpen}
+                />
+              )}
 
               <Popover.Button as="div" className="ham">
                 <img
@@ -59,6 +69,64 @@ const Navbar = ({
                 />
               </Popover.Button>
             </header>
+
+            {!breakpoint.sm && (
+              <menu
+                onMouseLeave={() => debouncedMouseLeave()}
+                className={`dropdown-menu service-${serviceMenuOpen ?? "none"}`}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    paddingTop: "100px",
+                  }}
+                >
+                  <img
+                    src="/images/Homepage/dropdown1.png"
+                    style={{
+                      height: "100%",
+                      objectFit: "cover",
+                      width: "45%",
+                    }}
+                  ></img>
+                  <ul
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      flexGrow: "1",
+                      flexShrink: "0",
+                      flexBasis: "1",
+                    }}
+                  >
+                    {services.map((item, index) => (
+                      <Link
+                        to={`/services/${_.kebabCase(item)}`}
+                        className="dropdown-item"
+                        style={{ width: "100%", display: "inline-block" }}
+                        key={index}
+                      >
+                        <li
+                          style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                          }}
+                        >
+                          <span>{item}</span>
+
+                          <img
+                            className="dropdown-arrow"
+                            src="/images/down-right 1.svg"
+                            alt="down-arrow"
+                          />
+                        </li>
+                      </Link>
+                    ))}
+                  </ul>
+                </div>
+              </menu>
+            )}
 
             {breakpoint.sm && (
               <Popover.Panel
