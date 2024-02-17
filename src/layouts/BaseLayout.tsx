83af51103c9457fde73f365d180512b5e730f0eb
Subrato-oid <subratochowdhury111@gmail.com>
2024-02-17 21:17:53 +0530

    Styling

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -2,11 +2,13 @@ import * as React from "react"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
 import "../styles/styles.scss"
+import Headroom from "react-headroom"
 
 type LayoutProp = {
   children: React.ReactNode
   pageName: string
 }
+
 const BaseLayout = ({ children, pageName }: LayoutProp): React.ReactElement => {
   const [mobileMenuActive, setMobileMenuActive] = React.useState<
   boolean | null
@@ -18,10 +20,12 @@ const BaseLayout = ({ children, pageName }: LayoutProp): React.ReactElement => {
       className={`${pageName} mobile-menu-${mobileMenuActive ?? "none"}`}
       style={mobileMenuActive ? {} : {}}
     >
-      <Navbar
-        mobileMenuActive={mobileMenuActive}
-        setMobileMenuActive={setMobileMenuActive}
-      />
+      <Headroom style={{ zIndex: "3" }}>
+        <Navbar
+          mobileMenuActive={mobileMenuActive}
+          setMobileMenuActive={setMobileMenuActive}
+        />
+      </Headroom>
       <div>{children}</div>
       <Footer />
     </div>
